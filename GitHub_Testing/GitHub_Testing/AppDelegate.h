87c51ccc336c961d2a843310bb7b9fad90d30ea7
LOGICSHORE <logicshore@LOGIC.local>
2018-04-06 15:08:34 +0530

    Changes Done

---
+++ GitHub_Testing/GitHub_Testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub_Testing
+//
+//  Created by LOGICSHORE on 06/04/18.
+//  Copyright © 2018 LOGICSHORE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
